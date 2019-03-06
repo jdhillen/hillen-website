4d48371e494a167a6cba0d5f52fdc223b41b4e1d
J.D. Hillen <jhillen@dimin.com>
2019-03-06 14:55:52 -0600

    Added - Full site navigation

--- src/components/layout/Navigation.vue
+++ src/components/layout/Navigation.vue
@@ -2,14 +2,16 @@
 <template>
     <nav class="nav">
 
-        <div class="grid-container">
-            <div class="grid-x">
-                <div class="cell">
-                    <router-link to="/">Home</router-link>
-                    <router-link to="/about">About</router-link>
-                    <router-link to="/resume">Resume</router-link>
-                </div>
-            </div>
+        <button class="hamburger hamburger--squeeze" :class="{ 'is-active': isActive }" @click="toggleNav()" type="button">
+            <span class="hamburger-box">
+                <span class="hamburger-inner"></span>
+            </span>
+        </button>
+
+        <div class="nav__container" :class="{ 'is-active': isActive }">
+            <router-link to="/" @click.native="toggleNav()">Home</router-link>
+            <router-link to="/about" @click.native="toggleNav()">About</router-link>
+            <router-link to="/resume" @click.native="toggleNav()">Resume</router-link>
         </div>
         
     </nav>
@@ -21,7 +23,66 @@
 
 export default {
     name: 'site-navigation',
-}
+
+    components: {
+
+    },
+
+    data() {
+        return {
+            isActive: false,
+        };
+    },
+
+    beforeCreate() {
+        
+    },
+
+    created() {
+
+    },
+
+    beforeMount() {
+        
+    },
+
+    mounted() {
+       
+    },
+
+    beforeUpdate() {
+
+    },
+
+    updated() {
+        
+    },
+
+    beforeDestroy() {
+    
+    },
+
+    destroyed() {
+
+    },
+
+    computed: {
+
+    },
+
+    methods: {
+
+        toggleNav: function() {
+            this.isActive = !this.isActive;
+        },
+
+    },
+
+    watch: {
+        
+    }
+    
+};
 
 </script>
 
@@ -30,7 +91,51 @@ export default {
 <style lang="scss" scoped>
 
 .nav {
+    position: relative;
     text-align: center;
+
+    .hamburger {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        z-index: 200;
+
+        &-inner,
+        &-inner::before,
+        &-inner::after {
+            background-color: black(0.70);
+        }
+
+        &.is-active {
+            .hamburger-inner,
+            .hamburger-inner::before,
+            .hamburger-inner::after {
+                background-color: $white;
+            }
+        }
+    }
+
+    &__container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background-color: black(0.75);
+        z-index: 100;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        visibility: hidden;
+        opacity: 0;
+        transition: opacity 0.25s;
+        -webkit-transition: opacity 0.25s;
+
+        &.is-active {
+            visibility: visible;
+            opacity: 1;
+        }
+    }
 }
 
 </style>
